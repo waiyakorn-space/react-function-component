004e03dd81d9878b374c04850cc49ad42040e9b6
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-05-31 16:34:13 +0700

    athandler

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import './App.css';
 function App() {
   const greeting = "Hello Function Component!"
 //const [state, setstate] = useState(initialState) this is a hook
-  const [mytitle,setMyTitle] = useState('Edit something...')
+  const [myTitle,setMyTitle] = useState('Edit something...')
+  const handlerChange = event =>setMyTitle(event.target.value);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {mytitle}
+          {myTitle}
         </p>
-
-        <input type="text" value={mytitle} onChange={event =>setMyTitle(event.target.value) } />
+        <input type="text" value={myTitle} onChange={handlerChange} />
       </header>
       <Header value={greeting}/>
     </div>
