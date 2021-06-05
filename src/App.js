52d8e04ef6f3be9f9a19b2b2e2b6371623b1d045
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-06-05 15:28:39 +0700

    forwardRef

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import {useState, memo} from 'react'
-import Headline from './Headline'
+import {useState, useEffect, useRef, forwardRef} from 'react'
 
 const App = () => {
-  const greeting = 'Hello Function Component'
+  const [greeting,setGreeting] = useState('Hello React!')
+
+  const handleChange = event => setGreeting(event.target.value);
+
+  const ref = useRef(); //ต้องการ focus ที่ component นี้
+  useEffect(() => ref.current.focus() ,[]);
 
   return (
     <div className="App">
-      <Headline value={greeting}/>
+      <h1>{greeting}</h1>
+      <Input value={greeting} handleChange={handleChange} ref={ref}/>
+
     </div>
   );
 }
+
+const Input = forwardRef(({value,handleChange},ref) =>(
+
+    <input type="text" value={value} onChange={handleChange} ref={ref} />
+
+));
 
 
 export default App;
