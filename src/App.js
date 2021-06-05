5e2aee2f1dfc89ec1114fefac592536ff2ec5337
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-06-05 15:03:29 +0700

    import

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import {useState, memo} from 'react'
+import Headline from './Headline'
 
 const App = () => {
-  /*const initialCount = +localStorage.getItem('storageCount') || 0;
-  const [myName,setMyName] = useState('Change my name');*/
-//const [state, setstate] = useState(initialState) this is a hook
-  const [count, setCount] = useState(0);
-  const [greeting,setGreeting] = useState('Hello React!')
-
-  const handlerIncrement = () => setCount(currentCount => currentCount+1); 
-  const handlerDecrement = () => setCount(currentCount => currentCount-1); 
-
-  const handlerChange = event => setGreeting(event.target.value);
-
-/*  useEffect(() => {
-    localStorage.setItem('storageCount',count)
-    console.log('updating when state changed.')
-  },[count])
-    ต้องการให้ทำงานทุกครั้ง           ไม่ต้องใส่ ,[]
-    เฉพาะตอน mount or unmount   ใส่ ,[]
-    เปลี่ยนตอนมีตัวแปรเปลี่ยน          ใส่ ,[variable]
-*/
-
+  const greeting = 'Hello Function Component'
 
   return (
     <div className="App">
-      <input type="text" onChange={handlerChange} />
-      <br />
-
-      <Count count={count}/>
-
-     <button type="button" onClick={handlerIncrement}>Increment</button>
-     <button type="button" onClick={handlerDecrement}>Decrement</button>
-
+      <Headline value={greeting}/>
     </div>
   );
 }
 
-const Count = memo(({count}) => {
-  console.log('Does it (re)render?');
-  return <h1>{count}</h1>
-})
+
 export default App;
---
+++ src/Headline.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function headline(props) {
+  return (
+    <div>
+      <h1>{props.value}</h1>
+    </div>
+  )
+}
+
+export default headline
