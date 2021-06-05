d439d977febcd3a9bedbf40d4a81eb94327a4bfd
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-06-05 14:49:46 +0700

    memo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import {useState, useEffect} from 'react'
+import {useState, memo} from 'react'
 
 const App = () => {
-  const initialCount = +localStorage.getItem('storageCount') || 0;
+  /*const initialCount = +localStorage.getItem('storageCount') || 0;
+  const [myName,setMyName] = useState('Change my name');*/
 //const [state, setstate] = useState(initialState) this is a hook
-  const [count, setCount] = useState(initialCount);
-  const [myName,setMyName] = useState('Change my name');
+  const [count, setCount] = useState(0);
+  const [greeting,setGreeting] = useState('Hello React!')
+
   const handlerIncrement = () => setCount(currentCount => currentCount+1); 
   const handlerDecrement = () => setCount(currentCount => currentCount-1); 
 
-  useEffect(() => {
+  const handlerChange = event => setGreeting(event.target.value);
+
+/*  useEffect(() => {
     localStorage.setItem('storageCount',count)
     console.log('updating when state changed.')
   },[count])
-/*  ต้องการให้ทำงานทุกครั้ง           ไม่ต้องใส่ ,[]
+    ต้องการให้ทำงานทุกครั้ง           ไม่ต้องใส่ ,[]
     เฉพาะตอน mount or unmount   ใส่ ,[]
     เปลี่ยนตอนมีตัวแปรเปลี่ยน          ใส่ ,[variable]
 */
@@ -20,14 +24,20 @@ const App = () => {
 
   return (
     <div className="App">
-     <h1>{count}</h1>
-     <h1>{myName}</h1>
+      <input type="text" onChange={handlerChange} />
+      <br />
+
+      <Count count={count}/>
+
      <button type="button" onClick={handlerIncrement}>Increment</button>
      <button type="button" onClick={handlerDecrement}>Decrement</button>
-     <br />
-     <button type='button' onClick={() => setMyName('Stan Smith')}>Change name</button>
+
     </div>
   );
 }
 
+const Count = memo(({count}) => {
+  console.log('Does it (re)render?');
+  return <h1>{count}</h1>
+})
 export default App;
